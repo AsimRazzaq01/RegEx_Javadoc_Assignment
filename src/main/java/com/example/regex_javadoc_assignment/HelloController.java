8e04a0b79e2425b41c9ce71bb564f34d2937af4a
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-04-13 16:10:09 -0400

    innit

---
+++ src/main/java/com/example/regex_javadoc_assignment/HelloController.java
@@ -0,0 +1,48 @@
+package com.example.regex_javadoc_assignment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+public class HelloController {
+    // GUI
+    // Text Fields
+    @FXML
+    private TextField DOB_txtfield;
+    @FXML
+    private TextField Email_txtfield;
+    @FXML
+    private TextField FirstName_txtfield;
+    @FXML
+    private TextField LastName_txtfield;
+    @FXML
+    private TextField ZipCode_txtfield;
+    // button
+    @FXML
+    private Button Register_Button;
+    // label
+    @FXML
+    private Label Label_txtField;
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
